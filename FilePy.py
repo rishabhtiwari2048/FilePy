45031c4320e4722686ff11e4054a75b5a172b9b4
Rishabh <rishabhtiwari2048@gmail.com>
2020-02-15 23:57:16 +0530

    demo

--- FilePy.py
+++ FilePy.py
@@ -1,4 +1,14 @@
 import cx_Oracle;
 def filepy():
-    print('Hello World')
+    print('Establishing database connection')
+    ## Step 1 Connect to database
+    username = "hr"
+    password = 123
+    connection = cx_Oracle.connect(username,password,"LAPTOP-DO3LVILU/XE", encoding = "utf-8")
+    print('Connection established successfully')
+    ## Step 2 Execute the query to get the list of community ids
+    ## Step 3 Get the data for each community and make a separate file of it
+    ## Repeat the step
+    ## Close the database connection
+    connection.close()
 filepy()
