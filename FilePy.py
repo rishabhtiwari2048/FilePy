28405559a5a6709cc5ec9b0bebdcd8d7f1ffff7a
Rishabh <rishabhtiwari2048@gmail.com>
2020-02-16 09:01:49 +0530

    Connection established successfully

--- FilePy.py
+++ FilePy.py
@@ -3,12 +3,13 @@ def filepy():
     print('Establishing database connection')
     ## Step 1 Connect to database
     username = "hr"
-    password = 123
-    connection = cx_Oracle.connect(username,password,"LAPTOP-DO3LVILU/XE", encoding = "utf-8")
+    password = "123"
+    connection = cx_Oracle.connect(username,password,"LAPTOP-DO3LVILU/XEPDB1", encoding = "utf-8")
     print('Connection established successfully')
     ## Step 2 Execute the query to get the list of community ids
     ## Step 3 Get the data for each community and make a separate file of it
     ## Repeat the step
     ## Close the database connection
     connection.close()
+    print("Connection closed successfully")
 filepy()
