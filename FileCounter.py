65dd8825e794b3e823eced44b76c351985fe0d36
Rishabh <rishabhtiwari2048@gmail.com>
2020-02-16 23:08:05 +0530

    Added following features 1) Differentiate approach based on record count 2) Improved memory optimization while execution 3) Modular approach

--- FileCounter.py
+++ FileCounter.py
@@ -1,1 +1,15 @@
-print("Hello World! I am file counter")
+import os
+
+if __name__ == "__main__":
+    result = next(os.walk('E:/',topdown=True))
+    print(result[1])
+
+    for i in result[1]:
+        print(i)
+        total = 0
+        for (root, subdir, files) in os.walk('E:/'+i):
+            total = total + len(files)
+        print(total)
+
+
+
