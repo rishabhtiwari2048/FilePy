7bb9b7a0bd481ae456df61c83f0990b52c336133
Rishabh <rishabhtiwari2048@gmail.com>
2020-02-16 12:28:07 +0530

    Added following features 1) Able to pick up distinct community ids 2) Able to create workbook and worksheet 3) Able to load data into worksheet 4) Format cells

--- FilePy.py
+++ FilePy.py
@@ -1,15 +1,57 @@
-import cx_Oracle;
+import cx_Oracle
+import xlsxwriter
+
+
 def filepy():
     print('Establishing database connection')
-    ## Step 1 Connect to database
     username = "hr"
     password = "123"
-    connection = cx_Oracle.connect(username,password,"LAPTOP-DO3LVILU/XEPDB1", encoding = "utf-8")
+    file_header = (
+        "department_id", "department_name", "manager_id", "location_id", "employee_id", "first_name", "last_name",
+        "email",
+        "hire_date", "salary")
+    connection = cx_Oracle.connect(username, password, "LAPTOP-DO3LVILU/XEPDB1", encoding="utf-8")
     print('Connection established successfully')
-    ## Step 2 Execute the query to get the list of community ids
-    ## Step 3 Get the data for each community and make a separate file of it
-    ## Repeat the step
-    ## Close the database connection
+    communities = connection.cursor()
+    records = connection.cursor()
+    for (community_id, community_name,) in communities.execute(
+            "SELECT distinct department_id, department_name FROM t_demo"):
+        workbook = xlsxwriter.Workbook("E:/A/" + community_name + ".xlsx",{'constant_memory':True})
+        print('****************************************')
+        print(community_name+".xls created successfully")
+        worksheet = workbook.add_worksheet(community_name)
+        row_num = 0
+        col_num = 0
+        print('-----------------------------------------')
+        print("writing data")
+        cell_format1 = workbook.add_format()
+        cell_format1.set_bold(True)
+        cell_format1.set_bg_color('cyan')
+        cell_format1.set_font_size(12)
+        cell_format1.set_font_color('black')
+        cell_format1.set_border(1)
+        cell_format1.set_font('Calibri')
+        worksheet.write_row(row_num,col_num,file_header,cell_format1)
+        row_num = row_num + 1
+        query = "SELECT * FROM t_demo WHERE department_id =" + str(community_id)
+        records.execute(query)
+        rows = records.fetchall()
+        cell_format2 = workbook.add_format()
+        cell_format2.set_border(1)
+        cell_format2.set_font_color('black')
+        cell_format2.set_font('Calibri')
+        cell_format2.set_font_size(10)
+        for row in rows:
+            worksheet.write_row(row_num,col_num,row,cell_format2)
+            row_num = row_num + 1
+        print(str(row_num)+" rows written")
+        print("writing complete")
+        print("------------------------------------------")
+        print("Closing "+community_name+".xls")
+        print('*******************************************')
+        workbook.close()
     connection.close()
     print("Connection closed successfully")
+
+
 filepy()
